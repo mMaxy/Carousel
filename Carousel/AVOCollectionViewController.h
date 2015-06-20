bcdb588ad6eb70acdc57a6156ee38c17cbd520a3
Artem Olkov <art.olkov@gmail.com>
2015-06-20 23:43:34 +0300

    + collection view + collection view cell + collors + straightforward placement fo cells in collection view

--- Carousel/AVOCollectionViewController.h
+++ Carousel/AVOCollectionViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AVOCollectionViewController.h
 //  Carousel
 //
 //  Created by Artem Olkov on 20/06/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface AVOCollectionViewController : UICollectionViewController <UICollectionViewDelegateFlowLayout>
 
 @end
-
