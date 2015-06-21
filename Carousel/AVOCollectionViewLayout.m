fce1c3dd89acee7fa3b07ee59118f225112533b0
Artem Olkov <art.olkov@gmail.com>
2015-06-21 13:31:06 +0300

    + Collection View Layout files

---
+++ Carousel/AVOCollectionViewLayout.m
@@ -0,0 +1,13 @@
+//
+//  AVOCollectionViewLayout.m
+//  Carousel
+//
+//  Created by Artem Olkov on 21/06/15.
+//  Copyright (c) 2015 aolkov. All rights reserved.
+//
+
+#import "AVOCollectionViewLayout.h"
+
+@implementation AVOCollectionViewLayout
+
+@end
