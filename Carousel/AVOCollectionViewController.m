1a17603e59290306d1127c500617555bd785b10a
Artem Olkov <art.olkov@gmail.com>
2015-06-22 08:26:29 +0300

    + some base implementation for layout - tap recognizer from controller

--- Carousel/AVOCollectionViewController.m
+++ Carousel/AVOCollectionViewController.m
@@ -106,56 +106,7 @@ static NSString * const reuseIdentifier = @"CarouselCell";
         [cell setBackgroundColor:[UIColor blueColor]];
     [cell.textLabel setText:[NSString stringWithFormat:@"%i", (indexPath.item + 1)]];
 
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapped)];
-    [cell addGestureRecognizer:tap];
-
     return cell;
-}
-
-- (void)tapped {
-
-    __weak typeof(self) weakSelf = self;
-    [self.collectionView performBatchUpdates:^{
-        __strong typeof(self) strongSelf = weakSelf;
-        if (strongSelf) {
-            NSIndexPath *indexPathFrom;
-            NSIndexPath *indexPathTo;
-            indexPathFrom = [NSIndexPath indexPathForItem:0 inSection:0];
-            indexPathTo = [NSIndexPath indexPathForItem:1 inSection:0];
-            [strongSelf.collectionView moveItemAtIndexPath:indexPathFrom toIndexPath:indexPathTo];
-
-            indexPathFrom = [NSIndexPath indexPathForItem:1 inSection:0];
-            indexPathTo = [NSIndexPath indexPathForItem:2 inSection:0];
-            [strongSelf.collectionView moveItemAtIndexPath:indexPathFrom toIndexPath:indexPathTo];
-
-            indexPathFrom = [NSIndexPath indexPathForItem:2 inSection:0];
-            indexPathTo = [NSIndexPath indexPathForItem:5 inSection:0];
-            [strongSelf.collectionView moveItemAtIndexPath:indexPathFrom toIndexPath:indexPathTo];
-
-            indexPathFrom = [NSIndexPath indexPathForItem:5 inSection:0];
-            indexPathTo = [NSIndexPath indexPathForItem:8 inSection:0];
-            [strongSelf.collectionView moveItemAtIndexPath:indexPathFrom toIndexPath:indexPathTo];
-
-            indexPathFrom = [NSIndexPath indexPathForItem:8 inSection:0];
-            indexPathTo = [NSIndexPath indexPathForItem:7 inSection:0];
-            [strongSelf.collectionView moveItemAtIndexPath:indexPathFrom toIndexPath:indexPathTo];
-
-            indexPathFrom = [NSIndexPath indexPathForItem:7 inSection:0];
-            indexPathTo = [NSIndexPath indexPathForItem:6 inSection:0];
-            [strongSelf.collectionView moveItemAtIndexPath:indexPathFrom toIndexPath:indexPathTo];
-
-            indexPathFrom = [NSIndexPath indexPathForItem:6 inSection:0];
-            indexPathTo = [NSIndexPath indexPathForItem:3 inSection:0];
-            [strongSelf.collectionView moveItemAtIndexPath:indexPathFrom toIndexPath:indexPathTo];
-
-            indexPathFrom = [NSIndexPath indexPathForItem:3 inSection:0];
-            indexPathTo = [NSIndexPath indexPathForItem:0 inSection:0];
-            [strongSelf.collectionView moveItemAtIndexPath:indexPathFrom toIndexPath:indexPathTo];
-        }
-    } completion:^(BOOL finished) {
-
-    }];
-
 }
 
 #pragma mark <UICollectionViewDelegateFlowLayout>
--- Carousel/AVOCollectionViewLayout.m
+++ Carousel/AVOCollectionViewLayout.m
@@ -10,4 +10,61 @@
 
 @implementation AVOCollectionViewLayout
 
+#pragma mark - UICollectionViewLayout Implementation
+
+- (CGSize)collectionViewContentSize {
+    // Don't scroll horizontally
+    CGFloat contentWidth = self.collectionView.bounds.size.width;
+
+    // Don't scroll vertically
+    CGFloat contentHeight = self.collectionView.bounds.size.height;
+
+    CGSize contentSize = CGSizeMake(contentWidth, contentHeight);
+    return contentSize;
+}
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSMutableArray *layoutAttributes = [NSMutableArray array];
+
+    // Cells
+    NSArray *visibleIndexPaths = [self indexPathsOfItemsInRect:rect];
+    for (NSIndexPath *indexPath in visibleIndexPaths) {
+        UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForItemAtIndexPath:indexPath];
+        [layoutAttributes addObject:attributes];
+    }
+
+    return layoutAttributes;
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewLayoutAttributes *attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+
+    CGRect frame = [self frameForCardAtIndex:indexPath.item];
+    attributes.frame = frame;
+
+    return attributes;
+}
+
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    return YES;
+}
+
+#pragma mark - Helpers
+
+- (NSArray *)indexPathsOfItemsInRect:(CGRect)rect {
+    NSArray *indexPaths;
+
+    //TODO
+
+    return indexPaths;
+}
+
+- (CGRect)frameForCardAtIndex:(NSInteger) index {
+    CGRect frame = CGRectZero;
+
+    //TODO
+
+    return frame;
+}
+
 @end
