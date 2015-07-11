6bfae9bfcf860f297b0d421e063673f3e25b009d
Artem Olkov <art.olkov@gmail.com>
2015-07-11 22:40:01 +0300

    + carousel vc * entry point of stotryboard moved to carousel vc

---
+++ Carousel/AVOCarouselViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVOCarouselViewController.h
+//  Carousel
+//
+//  Created by Artem Olkov on 11/07/15.
+//  Copyright (c) 2015 aolkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVOCarouselViewController : UIViewController 
+
+@end
